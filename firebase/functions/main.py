29b7c6f628b042638bb36376f3636ae2c472c169
Tom Moses <tom@tmoses.co.uk>
2023-12-05 18:44:21 +0000

    add firebase functions

--- firebase/functions/main.py
+++ firebase/functions/main.py
@@ -1,13 +1,40 @@
-# Welcome to Cloud Functions for Firebase for Python!
-# To get started, simply uncomment the below code or create your own.
+# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
 # Deploy with `firebase deploy`
+from firebase_functions import firestore_fn, https_fn
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+# The Firebase Admin SDK to access Cloud Firestore.
+from firebase_admin import initialize_app, firestore
+import google.cloud.firestore
 
-# initialize_app()
+app = initialize_app()
 
+@https_fn.on_request()
+def callback(req: https_fn.Request) -> https_fn.Response:
+    
+    # Grab the text parameter.
+    hub_challenge = req.args.get("hub.challenge")
+    if hub_challenge:
+        hub_verify_token = req.args.get("hub.verify_token")
+        if hub_verify_token == "test": # TODO: change to env variable
+            return https_fn.Response({ "hub.challenge":hub_challenge })
+        else:
+            return https_fn.Response("Wrong verify token", status=400)
+    else:
+        return https_fn.Response("No hub.challenge parameter provided", status=400)
 
-# @https_fn.on_request()
-# def on_request_example(req: https_fn.Request) -> https_fn.Response:
-#     return https_fn.Response("Hello world!")
+@https_fn.on_request()
+def addmessage(req: https_fn.Request) -> https_fn.Response:
+    """Take the text parameter passed to this HTTP endpoint and insert it into
+    a new document in the messages collection."""
+    # Grab the text parameter.
+    original = req.args.get("text")
+    if original is None:
+        return https_fn.Response("No text parameter provided", status=400)
+
+    firestore_client: google.cloud.firestore.Client = firestore.client()
+
+    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
+    _, doc_ref = firestore_client.collection("messages").add({"original": original})
+
+    # Send back a message that we've successfully written the message
+    return https_fn.Response(f"Message with ID {doc_ref.id} added.")
