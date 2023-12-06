e2a5a542cf9d9af02e61d8a060444b2edb1c65da
Tom Moses <tom@tmoses.co.uk>
2023-12-06 16:08:40 +0000

    add info to home screen

--- firebase/src/pages/UserHome.jsx
+++ firebase/src/pages/UserHome.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography } from 'antd';
+import { Typography, Paragraph } from 'antd';
 import { doc } from 'firebase/firestore';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 
@@ -20,6 +20,21 @@ const UserHome = (props) => {
           User data is incomplete.
         </Title>}
         {data.firstname && <Title level={3}>Welcome {data.firstname}</Title>}
+        <Paragraph>
+          How does this work? Good question. I'm not sure yet, but it'll probably be something like this:
+        </Paragraph>
+        <Paragraph>
+          <ol>
+            <li>Sign in with Strava ✅</li>
+            <li>Any activity updates get sent to this app ✅</li>
+            <li>Choose how you want to filter activities to go to UKC/UKH</li>
+            <li>2 options now:</li>
+            <ul>
+              <li>You can trust this site with your UKC/UKH login detials (they dont have an API 😔) and it can automatically add your activities to your UKC/UKH Activity Dairy (after an optional delay)</li>
+              <li>You can get an export of activties, ready to load into a local script that uploads them to UKC/UKH.</li>
+            </ul>
+          </ol>
+        </Paragraph>
       </>
     }
     </>
